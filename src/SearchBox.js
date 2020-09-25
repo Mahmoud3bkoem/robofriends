21aa257e151008ca0d8d049d6bd92d4ed8bef7ae
mahmoud <mahmoudbeyblad@gmail.com>
2020-09-25 16:44:19 +0200

    hey wassup

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const SearchBox= () => {
+const SearchBox= ( { searchChange } ) => {
     return (
         <div className="pa2">
-            <input type="search" placeholder="search robots" className="pa3 ba b--green bg-lightest-blue"></input>
+            <input 
+                type="search" 
+                placeholder="search robots" 
+                className="pa3 ba b--green bg-lightest-blue"
+                onChange={ searchChange }>
+            </input>
         </div>
     );
 }
